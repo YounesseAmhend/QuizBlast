82b0ed9c731fd8e338b25630d56859ee232f24e2
YounesseAmhend <younesseamhend2019@gmail.com>
2023-06-15 17:34:27 +0000

    getting ready for deployement

--- quizblast/settings.py
+++ quizblast/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'my-app/build/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'my-app/build/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
