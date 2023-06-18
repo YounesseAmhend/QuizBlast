81205e41c6185ac8e0895e1b7e1a7cd589585161
YounesseAmhend <younesseamhend2019@gmail.com>
2023-06-18 23:46:00 +0000

    switching to mysql

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,9 @@ echo "BUILD START"
 
 
 python3 -m pip install -r requirements.txt
+# Set MySQL/MariaDB configuration
+export MYSQL_CONFIG=mysql_config
+
 python3 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
